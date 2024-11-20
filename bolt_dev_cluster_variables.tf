7f916a5a19498ef4f7651748ed79744790d2e451
Tannvi T <45279580+Tannvi@users.noreply.github.com>
2024-11-20 16:41:32 +0000

    Add files via upload

---
+++ bolt_dev_cluster_variables.tf
@@ -0,0 +1,143 @@
+# VPC and Subnet Configuration
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "bolt-dev-vpc"
+  }
+}
+
+resource "aws_subnet" "private_zone1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = local.zone1
+
+  tags = {
+    "Name"                                                 = "${local.env}-private-${local.zone1}"
+    "kubernetes.io/role/internal-elb"                      = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_zone2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = local.zone2
+
+  tags = {
+    "Name"                                                 = "${local.env}-private-${local.zone2}"
+    "kubernetes.io/role/internal-elb"                      = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = local.zone1
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                 = "${local.env}-public-${local.zone1}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = local.zone2
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                 = "${local.env}-public-${local.zone2}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+
+#Internet Gateway Configuration
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "bolt-dev-igw"
+  }
+}
+
+
+
+#Natgateway Configuration
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "bolt-dev-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_zone1.id
+
+  tags = {
+    Name = "bolt-dev-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+#Route Table COnfiguration
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "bolt-dev-private-rt"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "bolt-dev-public-rt"
+  }
+}
+
+
+#Route Table association with subnett configuration
+resource "aws_route_table_association" "private_zone1" {
+  subnet_id      = aws_subnet.private_zone1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_zone2" {
+  subnet_id      = aws_subnet.private_zone2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public_zone1" {
+  subnet_id      = aws_subnet.public_zone1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_zone2" {
+  subnet_id      = aws_subnet.public_zone2.id
+  route_table_id = aws_route_table.public.id
+}
+
